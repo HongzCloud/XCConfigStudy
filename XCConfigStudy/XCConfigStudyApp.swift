a6582915280356f76a75769c59c7d7e8fbd72afb
HongzCloud <jihong21511@gmail.com>
2024-02-02 22:55:28 +0900

    Initial Commit

---
+++ XCConfigStudy/XCConfigStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCConfigStudyApp.swift
+//  XCConfigStudy
+//
+//  Created by 오킹 on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct XCConfigStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
